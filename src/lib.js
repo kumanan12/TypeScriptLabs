9d36129038a9ca7b2ff955bfa3d03078b2da4536
Kumanan Murugesan <kumanan@Kumanans-MacBook-Pro.local>
2016-10-12 23:20:51 -0400

    Removed highlighter, added toc.html , modified modules and templates.

--- src/lib.js
+++ src/lib.js
@@ -1,4 +1,8 @@
 "use strict";
+//private function
+function youCantCallMe() {
+    toastr.info("Inside youCantCallMe ");
+}
 exports.sqrt = Math.sqrt;
 function square(x) {
     return x * x;
--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,10 @@
-export const sqrt=Math.sqrt;
+declare var toastr;
+//private function
+function youCantCallMe() {
+    toastr.info("Inside youCantCallMe ");
+}
+export const sqrt = Math.sqrt;
 
-export function square(x){
+export function square(x) {
     return x * x;
 }
