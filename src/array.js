35daa3819c0ff5f846954c530f6b9c84036b4ed4
Kumanan Murugesan <kumanan@Kumanans-MacBook-Pro.local>
2016-10-18 21:05:03 -0400

    Started array demo.

---
+++ src/array.js
@@ -0,0 +1,2 @@
+var numbers = [10, 20, 30, 40, 50, 60, 70];
+log(numbers.find(function (t) { return t > 30; }));
---
+++ src/array.ts
@@ -0,0 +1,5 @@
+// toastr.info("Hello Array");
+declare var log;
+let numbers = [ 10, 20, 30, 40, 50, 60, 70];
+
+log(numbers.find(t => t> 30 ));
